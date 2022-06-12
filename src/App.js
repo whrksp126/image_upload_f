3f7709d60dac0428743156e3994be422ce958697
whrksp126 <whrksp126@naver.com>
2022-06-12 22:52:37 +0900

    react-router-dom 적용

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react"
-import UploadForm from "./components/UploadForm";
 // npm i react-toastify (alert을 이쁘게 해주는 라이브러리)
 import { ToastContainer } from 'react-toastify';
 // toastify를 사용하기 위해 css를 import 해야함
 import 'react-toastify/dist/ReactToastify.css';
-import ImageList from "./components/ImageList";
+import LoginPage from "./pages/LoginPage";
+import MainPage from "./pages/MainPage";
+import RegisterPage from "./pages/RegisterPage";
+// npm i react-router-dom (페이지를 만들기 위해서)
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div style={{maxWidth: 600, margin:"auto"}}>
       {/* toastify를 전역에서 사용할 수 있게함 */}
       <ToastContainer />
-      <h2>사진첩</h2>
-      <UploadForm />
-      <ImageList />
+        <Routes>
+          {/* exact는 완전 동일한 url 일때만 해당 컴포넌트를 보여줘라는 의미 */}
+          <Route path="/auth/register" exact element={<RegisterPage />} />
+          <Route path="/auth/login" exact element={<LoginPage />} />
+          <Route path="/" element={<MainPage />} />
+        </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ImageProvider } from './context/ImageContext';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ImageProvider>
-      <App />
-    </ImageProvider>
+    <BrowserRouter>
+      <ImageProvider>
+        <App />
+      </ImageProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
