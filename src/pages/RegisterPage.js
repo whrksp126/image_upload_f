6809314f6abb0b3df29cbf8303cebc0c96fad083
whrksp126 <whrksp126@naver.com>
2022-06-14 23:59:53 +0900

    form api setting_1

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,23 @@
 import React, { useState } from 'react'
 import CustomInput from '../components/CustomInput'
+import { toast } from 'react-toastify'
 
 const RegisterPage = () => {
   const [name, setName] = useState('');
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [passwordCheck, setPasswordCheck] = useState('');
+
+  const submitHandler = (e) => {
+    try{
+      e.preventDefault();
+      console.log({name, userName, password, passwordCheck})
+
+    } catch(err){
+      console.error(err);
+      toast.err(err.message)
+    }
+  }
   return (
     <div
       style={{
@@ -16,12 +28,13 @@ const RegisterPage = () => {
       }}
     >
       <h3>회원가입</h3>
-      <from>
+      <form onSubmit={submitHandler}>
         <CustomInput label="이름" value={name} setValue={setName} />
         <CustomInput label="회원ID" value={userName} setValue={setUserName} />
         <CustomInput label="비밀번호" value={password} setValue={setPassword} type="password" />
         <CustomInput label="비밀번호 확인" value={passwordCheck} setValue={setPasswordCheck} type="password" />
-      </from>
+        <button type="submit">회원가입</button>
+      </form>
     </div>
   )
 }
